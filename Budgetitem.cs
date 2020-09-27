e817845a9963eaa623105d2f334a07163e703961
우영 박 <lamnun@naver.com>
2020-09-28 00:13:31 +0900

    폼 블레이저

---
+++ Budgetitem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp
+{
+    public class Budgetitem
+    {
+
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Models/ExampleModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class ExampleModel
+    {
+        [Required(ErrorMessage ="이름은 필수입니다.")]
+        [StringLength(10, ErrorMessage = "이름을 10자 이하로 입력하세요")]
+        public string Name { get; set; }
+    }
+}
